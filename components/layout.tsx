5abfd7bcc1e95b4e76dd3fa0d1279cd7a2e696c7
anunay <anusu90@gmail.com>
2022-01-18 16:09:37 +0530

    added GA

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,48 +1,50 @@
-import { FC, useCallback } from 'react'
-import Script from 'next/script'
-import { Layout, Page } from '@vercel/examples-ui'
-import type { LayoutProps } from '@vercel/examples-ui/layout'
-import { GaProvider } from '@lib/useGa'
+import React, { FC, useCallback } from "react";
+import Script from "next/script";
+import { Layout, Page } from "@vercel/examples-ui";
+import type { LayoutProps } from "@vercel/examples-ui/layout";
+import { GaProvider } from "@lib/useGa";
 
 function throwIfSSR() {
-  throw new Error('Using GA during SSR is not allowed')
+  throw new Error("Using GA during SSR is not allowed");
 }
 
 function gaHandler() {
   const dataLayer = ((window as any).dataLayer =
-    (window as any).dataLayer || [])
+    (window as any).dataLayer || []);
 
-  dataLayer.push(arguments)
+  dataLayer.push(arguments);
 }
 
 const OptimizeLayout: FC<LayoutProps> = ({ children, ...props }) => {
   const ga = useCallback(
-    typeof window === 'undefined' ? throwIfSSR : gaHandler,
+    typeof window === "undefined" ? throwIfSSR : gaHandler,
     []
-  )
+  );
 
   return (
     <Layout {...props}>
       <Page>
-        {/* <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID}`}
-        onLoad={() => {
-          window.dataLayer = window.dataLayer || []
-          function gtag() {
-            dataLayer.push(arguments)
-          }
-
-          gtag('js', new Date())
-          gtag('config', process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID)
-        }}
-      /> */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID}`}
+          strategy="lazyOnload"
+        ></Script>
+        <Script strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+                `}
+        </Script>
         <Script
           src={`https://www.googleoptimize.com/optimize.js?id=${process.env.NEXT_PUBLIC_OPTIMIZE_CONTAINER_ID}`}
         />
         <GaProvider value={ga}>{children}</GaProvider>
       </Page>
     </Layout>
-  )
-}
+  );
+};
 
-export default OptimizeLayout
+export default OptimizeLayout;
